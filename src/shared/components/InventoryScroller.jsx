9377eb0c3e2e5790ddcaa17525d9d705a89ad42c
Gabe Wilmoth <35712811+GabeWilmoth@users.noreply.github.com>
2024-09-18 08:43:50 -0500

    Fixing Console Errors & Setting Dynamic Reviews

--- src/shared/components/InventoryScroller.jsx
+++ src/shared/components/InventoryScroller.jsx
@@ -6,55 +6,55 @@ export default function InventoryScroller() {
         "title": "1994 Honda Attack",
         "price": 6800,
         "mileage": 56000,
-        "link": "/4Y1SL65848Z411439",
+        "link": "/4Y1SL65848Z4114392",
     },
     {
         "title": "1990 Suzuki Carry",
         "price": 6800,
         "mileage": 56000,
-        "link": "/4Y1SL65848Z411439",
+        "link": "/4Y1SL65848Z4114393",
     },
     {
         "title": "1990 Suzuki Carry",
         "price": 6800,
         "mileage": 56000,
-        "link": "/4Y1SL65848Z411439",
+        "link": "/4Y1SL65848Z4114394",
     },
     {
         "title": "1990 Suzuki Carry",
         "price": 6800,
         "mileage": 56000,
-        "link": "/4Y1SL65848Z411439",
+        "link": "/4Y1SL65848Z4114395",
     },
     {
         "title": "1990 Suzuki Carry",
         "price": 6800,
         "mileage": 56000,
-        "link": "/4Y1SL65848Z411439",
+        "link": "/4Y1SL65848Z4114396",
     },
     {
         "title": "1990 Suzuki Carry",
         "price": 6800,
         "mileage": 56000,
-        "link": "/4Y1SL65848Z411439",
+        "link": "/4Y1SL65848Z4114397",
     },
     {
         "title": "1990 Suzuki Carry",
         "price": 6800,
         "mileage": 56000,
-        "link": "/4Y1SL65848Z411439",
+        "link": "/4Y1SL65848Z4114398",
     },
     {
         "title": "1990 Suzuki Carry",
         "price": 6800,
         "mileage": 56000,
-        "link": "/4Y1SL65848Z411439",
+        "link": "/4Y1SL65848Z4114399",
     }];
 
     return (
         <div className="bg-grey-footer p-2 rounded-lg grid auto-cols grid-flow-col overflow-x-auto gap-2">
             {props.map((inventoryItem) => (
-                <a href={inventoryItem.link}>
+                <a key={inventoryItem.link} href={inventoryItem.link}>
                     <InventoryCard title={inventoryItem.title} price={inventoryItem.price} mileage={inventoryItem.mileage} />
                 </a>
             ))}
