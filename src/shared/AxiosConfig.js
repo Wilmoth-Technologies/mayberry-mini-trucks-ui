e7b5003c4844417c22d31ab7c3cdb6bdad13ccfb
Gabe Wilmoth <35712811+GabeWilmoth@users.noreply.github.com>
2024-10-22 23:13:34 -0400

    Update AxiosConfig.js

--- src/shared/AxiosConfig.js
+++ src/shared/AxiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://mayberry-mini-trucks-api-dev-795613298614.us-east4.run.app',
+  baseURL: 'https://mayberry-mini-trucks-api.dev.kubernetes-wilmoth.com',
   timeout: 10000, //10 Second Request Timeout
   headers: {
     'Content-Type': 'application/json',
