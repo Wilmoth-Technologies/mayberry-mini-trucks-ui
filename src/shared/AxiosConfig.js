b2a212b830767e0335bf6557f8c3d7ede682112f
Gabe Wilmoth <35712811+GabeWilmoth@users.noreply.github.com>
2024-11-14 07:21:16 -0600

    Update AxiosConfig.js

--- src/shared/AxiosConfig.js
+++ src/shared/AxiosConfig.js
@@ -12,16 +12,23 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
+
+
 // Add a request interceptor to add CSRF token to all non-GET requests
 axiosInstance.interceptors.request.use(async (config) => {
   // Only add CSRF token for state-changing methods
+  console.log("config: ", config);
+  console.log("HERE PRIOR");
   if (config.method !== 'get') {
     try {
+      console.log("HERE AFTER");
       // Get the CSRF token from the cookie if it exists
       const csrfToken = document.cookie
         .split('; ')
         .find(row => row.startsWith('PLAY_CSRF_TOKEN'))
         ?.split('=')[1];
+
+        console.log(csrfToken);
 
       if (csrfToken) {
         config.headers['Csrf-Token'] = csrfToken;
