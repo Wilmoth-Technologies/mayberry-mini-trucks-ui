8f8d4e89e7e65d2b3743e556eab8cf80c0452903
Gabe Wilmoth <35712811+GabeWilmoth@users.noreply.github.com>
2024-11-15 21:53:57 -0500

    Compressed Photos

--- src/shared/AxiosConfig.js
+++ src/shared/AxiosConfig.js
@@ -2,10 +2,9 @@
 import axios from 'axios';
 
 //Local Backend URL: http://localhost:9000
-
 const axiosInstance = axios.create({
   baseURL: 'https://api.mayberryminitrucks.com',
-  timeout: 45000, //30 Second Request Timeout
+  timeout: 45000, //45 Second Request Timeout
   headers: {
     'Content-Type': 'application/json',
   },
@@ -17,20 +16,13 @@ const axiosInstance = axios.create({
 // Add a request interceptor to add CSRF token to all non-GET requests
 axiosInstance.interceptors.request.use(async (config) => {
   // Only add CSRF token for state-changing methods
-  console.log("config: ", config);
-  console.log("HERE PRIOR");
   if (config.method !== 'get') {
     try {
-      console.log("HERE AFTER");
-      // Log the cookies to verify availability
-      console.log("Cookies available: ", document.cookie);
       // Get the CSRF token from the cookie if it exists
       const csrfToken = document.cookie
         .split('; ')
         .find(row => row.startsWith('PLAY_CSRF_TOKEN'))
         ?.split('=')[1];
-
-        console.log(csrfToken);
 
       if (csrfToken) {
         config.headers['Csrf-Token'] = csrfToken;
